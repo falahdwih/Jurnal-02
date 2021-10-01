833214267f2fb16edd70dc1965e4f197c990a2a0
fale-xy <91298490+fale-xy@users.noreply.github.com>
2021-10-01 12:47:25 +0700

    HelloWorld.java

---
+++ DemoGit/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main (String[] args) {
+        System.out.println("Nama : Falah Dwihandoyo");
+		System.out.println("NIM : 6706213019");
+		System.out.println("Kelas : D3-RPLA-4502");
+    }
+}
